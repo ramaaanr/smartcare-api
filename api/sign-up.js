917a3d197d5b91d4c6003c80b1f38f532549fb0a
Rama Noor Rizki <ramanoorrizki@gmail.com>
2022-11-29 21:56:28 +0800

    Add Firebase Config

--- api/sign-up.js
+++ api/sign-up.js
@@ -2,16 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { initializeApp } = require('firebase/app');
 const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
+const firebaseConfig = require('../config/firebase-config');
 
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAGNFcaaxQvge7U4xf1p4HnDtLFhrFgzZE',
-  authDomain: 'smartcare-3670b.firebaseapp.com',
-  projectId: 'smartcare-3670b',
-  storageBucket: 'smartcare-3670b.appspot.com',
-  messagingSenderId: '528534664501',
-  appId: '1:528534664501:web:937fa7e7047c5225e8c87c',
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
